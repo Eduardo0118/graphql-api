efc5ff0f4022cea3932718d6da047d1c510756cd
Eduardo0118 <pro-cs-4@hotmail.com>
2023-09-21 11:47:50 -0300

    Add db connection

---
+++ src/models/index.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { DataTypes, Sequelize } from 'sequelize';
+
+import { DbConnection } from '../interfaces/DbConnectionInterface';
+
+const basename: string = path.basename(module.filename);
+const env: string = process.env.NODE_ENV || 'development';
+
+let config = require(path.resolve(`${__dirname}./../config/config.json`))[env];
+
+let db = null;
+
+if (!db) {
+    db = {}
+
+    const sequelize: Sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config
+    );
+
+    fs.readdirSync(__dirname)
+      .filter((file: string) => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+      })
+      .forEach((file: string) => {
+        const modelDefiner = require(path.join(__dirname, file));
+        const model = modelDefiner(sequelize, DataTypes);
+        db[model.name] = model;
+      })
+
+      Object.keys(db).forEach((modelName: string) => {
+        if (db[modelName].associate) {
+          db[modelName].associate(db);
+        }
+      });
+
+      db.sequelize = sequelize;
+      db.Sequelize = Sequelize;
+}
+
+export default <DbConnection>db
